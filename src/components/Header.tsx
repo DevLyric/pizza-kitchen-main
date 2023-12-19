f461f9c3df97737bdf094bd7ad9f44801975ac56
Douglas Souza <lyricsan@hotmail.com>
2023-12-19 00:03:57 -0300

    adding sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.svg";
 import moon from "../assets/images/icon-moon.svg";
 import hamburger from "../assets/images/icon-hamburger.svg";
 
 function Header() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  useEffect(() => {
+    function changeWidth() {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", changeWidth);
+
+    if (screenWidth > 1024) {
+      setShowSidebar(false);
+    }
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, [screenWidth]);
+
+  function toggleSidebar() {
+    setShowSidebar((current) => !current);
+  }
+
   return (
     <header className="flex items-center h-24 shadow-md px-5">
       <div className="container mx-auto flex items-center justify-between">
@@ -39,12 +63,37 @@ function Header() {
           </div>
           <img src={moon} className="cursor-pointer z-50" width={28.2} alt="" />
           <img
+            onClick={toggleSidebar}
             src={hamburger}
             className="cursor-pointer z-50 lg:hidden"
             alt=""
           />
         </div>
       </div>
+      {showSidebar && (
+        <div className="absolute top-0 right-0 h-screen w-2/3 bg-orange-500">
+          <div className="flex flex-col gap-8 mt-52 mx-8">
+            <Link
+              to="/contact"
+              className="w-max tracking-wider font-medium hover:text-white"
+            >
+              CONTACT
+            </Link>
+            <Link
+              to="/about"
+              className="w-max tracking-wider font-medium hover:text-white"
+            >
+              ABOUT US
+            </Link>
+            <Link
+              to="/login"
+              className="w-max tracking-wider font-medium hover:text-white"
+            >
+              SIGNUP/LOGIN
+            </Link>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
