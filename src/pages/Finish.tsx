70783999db793d737eda15abcb99bfa832365546
Douglas Souza <lyricsan@hotmail.com>
2023-12-22 11:11:12 -0300

    styling and displaying rating card

--- src/pages/Finish.tsx
+++ src/pages/Finish.tsx
@@ -4,9 +4,11 @@ import pizza2 from "../assets/images/pizza-dark-2.png";
 import pizza3 from "../assets/images/pizza-dark-3.png";
 import pizza4 from "../assets/images/pizza-dark-4.png";
 import arrow from "../assets/images/up-arrow-black.png";
+import RatingCard from "../components/RatingCard";
 
 function Finish() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [showRating, setShowRating] = useState(false);
 
   const pizzaImages = [
     {
@@ -42,16 +44,16 @@ function Finish() {
       <div className="max-w-xl text-center">
         <h1
           style={{ fontSize: "clamp(2.25rem, 3vw, 3rem)" }}
-          className="font-bold"
+          className="font-bold pb-8"
         >
           Awesome, your order is on the way!
         </h1>
-        <p className="text-2xl">You ordered:</p>
+        <p className="text-2xl pb-2">You ordered:</p>
         <p className="tracking-wider">
           A pizza with chicago-style base and the following toppings: parma ham,
           pepperoni, extra cheese.
         </p>
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center my-10">
           {pizzaImages.map((pizza, index) => (
             <img
               key={index}
@@ -66,10 +68,21 @@ function Finish() {
         </div>
         <div className="flex items-center justify-between">
           <img src={arrow} width={100} alt="" />
-          <p>If you got a minute, tell us how we did!</p>
+          <p>
+            If you got a minute, tell us{" "}
+            <button
+              onClick={() => {
+                setShowRating(true);
+              }}
+              className="text-orange-500"
+            >
+              how we did!
+            </button>
+          </p>
           <img src={arrow} className="-scale-x-100" width={100} alt="" />
         </div>
       </div>
+      {showRating && <RatingCard />}
     </div>
   );
 }
