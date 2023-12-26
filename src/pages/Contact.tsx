a5c97f0076d100ff38a9928ce9d663e060d3594e
Douglas Souza <lyricsan@hotmail.com>
2023-12-26 12:02:03 -0300

    inputs and footer added

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,9 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FormDataProps } from "../@types/formData";
+import facebook from "../assets/images/icon-facebook.svg";
+import twitter from "../assets/images/icon-twitter.svg";
+import instagram from "../assets/images/icon-instagram.svg";
+import pinterest from "../assets/images/icon-pinterest.svg";
 
 function Contact() {
   const [formData, setFormData] = useState<FormDataProps>({
@@ -15,15 +19,22 @@ function Contact() {
     setFocusedInput(field);
   }
 
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    console.log("working");
+  }
+
   return (
-    <div className="flex justify-center mt-12 px-6">
-      <div className="w-full max-w-xl">
-        <h1 className="font-bold text-3xl mb-4">GET IN TOUCH</h1>
-        <p className="tracking-wider">
+    <div className="flex flex-col items-center mt-20">
+      <div className="w-full max-w-xl px-6">
+        <h1 className="font-bold text-3xl mb-4 text-center sm:text-left">
+          GET IN TOUCH
+        </h1>
+        <p className="mb-4 tracking-wider text-center sm:text-left">
           Have a question or feedback? We would love to hear from you!
         </p>
 
-        <form action="">
+        <form action="" onSubmit={handleSubmit} className="flex flex-col gap-6">
           <div
             className={`relative border-[3px] border-dashed rounded ${
               focusedInput === "name" || formData.name
@@ -40,6 +51,7 @@ function Contact() {
               First Name:
             </label>
             <input
+              id="name"
               type="text"
               className="w-full p-4 border-2 border-black rounded outline-none"
               value={formData.name}
@@ -48,10 +60,156 @@ function Contact() {
               }
               onFocus={() => handleFocus("name")}
               onBlur={() => setFocusedInput("")}
+              autoComplete="off"
+              required
             />
           </div>
+          <div
+            className={`relative border-[3px] border-dashed rounded ${
+              focusedInput === "email" || formData.email
+                ? "border-orange-500"
+                : "border-transparent"
+            }`}
+          >
+            <label
+              htmlFor="email"
+              className={`absolute left-4 transform -translate-y-1/2 text-orange-500 tracking-wider bg-white font-medium
+               ${focusedInput === "email" || formData.email ? "" : "top-1/2"}
+               `}
+            >
+              Email:
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="w-full p-4 border-2 border-black rounded outline-none"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              onFocus={() => handleFocus("email")}
+              onBlur={() => setFocusedInput("")}
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div
+            className={`relative border-[3px] border-dashed rounded ${
+              focusedInput === "subject" || formData.subject
+                ? "border-orange-500"
+                : "border-transparent"
+            }`}
+          >
+            <label
+              htmlFor="subject"
+              className={`absolute left-4 transform -translate-y-1/2 text-orange-500 tracking-wider bg-white font-medium
+               ${
+                 focusedInput === "subject" || formData.subject ? "" : "top-1/2"
+               }
+               `}
+            >
+              Subject:
+            </label>
+            <input
+              id="subject"
+              type="text"
+              className="w-full p-4 border-2 border-black rounded outline-none"
+              value={formData.subject}
+              onChange={(e) =>
+                setFormData({ ...formData, subject: e.target.value })
+              }
+              onFocus={() => handleFocus("subject")}
+              onBlur={() => setFocusedInput("")}
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div
+            className={`relative flex border-[3px] border-dashed rounded ${
+              focusedInput === "message" || formData.message
+                ? "border-orange-500"
+                : "border-transparent"
+            }`}
+          >
+            <label
+              htmlFor="message"
+              className={`absolute left-4 transform -translate-y-1/2 text-orange-500 tracking-wider bg-white font-medium
+               ${
+                 focusedInput === "message" || formData.message ? "" : "top-1/2"
+               }
+               `}
+            >
+              Message:
+            </label>
+            <textarea
+              id="message"
+              className="w-full p-4 border-2 border-black rounded outline-none"
+              value={formData.message}
+              onChange={(e) =>
+                setFormData({ ...formData, message: e.target.value })
+              }
+              onFocus={() => handleFocus("message")}
+              onBlur={() => setFocusedInput("")}
+              autoComplete="off"
+              required
+            />
+          </div>
+          <button className="self-center sm:self-start py-2 w-28 rounded font-medium bg-orange-500">
+            Send
+          </button>
         </form>
       </div>
+      <footer className="w-full mt-20 py-8 flex flex-col gap-8 bg-orange-500 lg:flex-row lg:justify-around lg:py-0">
+        <div className="flex flex-col items-center gap-8 lg:flex-row lg:gap-16 lg:py-8">
+          <div className="flex flex-col text-center lg:text-left">
+            <h1 className="my-2 text-xl text-white font-semibold">CALL US</h1>
+            <span className="text-white font-medium">+34 108 231 4622</span>
+            <span className="text-white font-medium">+34 127 220 3094</span>
+          </div>
+
+          <div className="flex flex-col text-center lg:text-left">
+            <h1 className="my-2 text-xl text-white font-semibold lg:my-0 lg:mb-4">
+              LOCATION
+            </h1>
+            <span className="text-white font-medium">
+              35 Main St, The City, TH 4502 H2
+            </span>
+          </div>
+
+          <div className="flex flex-col text-center lg:text-left">
+            <h1 className="my-2 text-xl text-white font-semibold">
+              BUSINESS HOURS
+            </h1>
+            <span className="text-white font-medium">Monday: Closed</span>
+            <span className="text-white font-medium">
+              Tuesday - Sunday: 11am - 22pm
+            </span>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center gap-6">
+          <img
+            src={facebook}
+            className="w-6 cursor-pointer hover:scale-110 invert"
+            alt=""
+          />
+          <img
+            src={twitter}
+            className="w-6 cursor-pointer hover:scale-110 invert"
+            alt=""
+          />
+          <img
+            src={instagram}
+            className="w-6 cursor-pointer hover:scale-110 invert"
+            alt=""
+          />
+          <img
+            src={pinterest}
+            className="w-6 cursor-pointer hover:scale-110 invert"
+            alt=""
+          />
+        </div>
+      </footer>
     </div>
   );
 }
