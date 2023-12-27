2ce34085b0c2b108dd69b3b05e5c573cffd5de0d
Douglas Souza <lyricsan@hotmail.com>
2023-12-27 13:59:29 -0300

    fix: padding added

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,6 +1,6 @@
 function Popup({ name }: { name: string }) {
   return (
-    <div className="absolute h-screen w-screen top-0 flex items-center justify-center bg-gray-500/50">
+    <div className="absolute h-screen w-screen top-0 flex items-center justify-center bg-gray-500/50 px-6">
       <div className="w-[400px] h-72 text-center py-16 px-5 bg-white border-2 border-dashed border-orange-500 rounded-2xl">
         <h1 className="text-2xl font-semibold">Thank you, {name}</h1>
         <p className="mt-4 mb-10 text-[#a2aab6]">
